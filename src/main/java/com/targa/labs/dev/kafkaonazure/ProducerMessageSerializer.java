50593c28345c3d24b0eaf0958c1ea53f9804aa6c
AlexanderTN <nguyenhoangtam@me.com>
2022-08-20 23:57:35 +0700

    Delete as I cannot commit and push from local atm

--- src/main/java/com/targa/labs/dev/kafkaonazure/ProducerMessageSerializer.java
+++
@@ -1,6 +0,0 @@
-package com.targa.labs.dev.kafkaonazure;
-
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-public class ProducerMessageSerializer extends JsonSerializer<SimpleMessage> {
-}
