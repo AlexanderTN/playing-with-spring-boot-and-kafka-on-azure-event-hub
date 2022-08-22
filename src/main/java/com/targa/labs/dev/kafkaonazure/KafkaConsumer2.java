145d9210c2442a6dfd75b35ede5ce0e3ca223a46
AlexanderTN <nguyenhoangtam@me.com>
2022-08-22 09:27:44 +0700

    Revert config2/consumer2

--- src/main/java/com/targa/labs/dev/kafkaonazure/KafkaConsumer2.java
+++ src/main/java/com/targa/labs/dev/kafkaonazure/KafkaConsumer2.java
@@ -1,15 +1,15 @@
-//package com.targa.labs.dev.kafkaonazure;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@Slf4j
-//public class KafkaConsumer2 {
-//
-//    @KafkaListener(topics = "${demo2.topic.name}", groupId="demo2", containerFactory = "kafkaListenerContainerFactory2")
-//    public void receive(SimpleMessage consumerMessage) {
-//        log.info("Received message from kafka queue: {}", consumerMessage.getBody());
-//    }
-//}
+package com.targa.labs.dev.kafkaonazure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaConsumer2 {
+
+    @KafkaListener(topics = "${demo2.topic.name}", groupId="demo2", containerFactory = "kafkaListenerContainerFactory2")
+    public void receive(SimpleMessage consumerMessage) {
+        log.info("Received message from kafka queue: {}", consumerMessage.getBody());
+    }
+}
--- src/main/java/com/targa/labs/dev/kafkaonazure/KafkaConsumerConfig2.java
+++ src/main/java/com/targa/labs/dev/kafkaonazure/KafkaConsumerConfig2.java
@@ -1,60 +1,60 @@
-//package com.targa.labs.dev.kafkaonazure;
-//
-//import org.apache.kafka.clients.consumer.ConsumerConfig;
-//import org.apache.kafka.common.serialization.StringDeserializer;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.kafka.annotation.EnableKafka;
-//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-//import org.springframework.kafka.config.KafkaListenerContainerFactory;
-//import org.springframework.kafka.core.ConsumerFactory;
-//import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-//import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@EnableKafka
-//@Configuration
-//public class KafkaConsumerConfig2 {
-//
-//    @Value(value = "${demo2.kafka.bootstrap-servers}")
-//    private String bootstrapAddress;
-//
-//    @Value("${demo2.kafka.properties.sasl.jaas.config}")
-//    private String kafkaSaslJaasConfig;
-//    @Value("${demo2.kafka.properties.sasl.mechanism}")
-//    private String kafkaSaslMechanism;
-//    @Bean
-//    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, String>>
-//    kafkaListenerContainerFactory2() {
-//        ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
-//                new ConcurrentKafkaListenerContainerFactory<>();
-//        factory.setConsumerFactory(consumerFactory2());
-//        factory.setConcurrency(3);
-//        factory.getContainerProperties().setPollTimeout(3000);
-//        return factory;
-//    }
-//
-//    @Bean
-//    public ConsumerFactory<Integer, String> consumerFactory2() {
-//        return new DefaultKafkaConsumerFactory<>(consumerConfigs2());
-//    }
-//
-//    @Bean
-//    public Map<String, Object> consumerConfigs2() {
-//        Map<String, Object> props = new HashMap<>();
-//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-//        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, "20971520");
-//        props.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, "20971520");
-//
-//        props.put("security.protocol", "SASL_SSL");
-//        props.put("sasl.jaas.config", this.kafkaSaslJaasConfig);
-//        props.put("sasl.mechanism", this.kafkaSaslMechanism);
-//
-//        return props;
-//    }
-//}
+package com.targa.labs.dev.kafkaonazure;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig2 {
+
+    @Value(value = "${demo2.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Value("${demo2.kafka.properties.sasl.jaas.config}")
+    private String kafkaSaslJaasConfig;
+    @Value("${demo2.kafka.properties.sasl.mechanism}")
+    private String kafkaSaslMechanism;
+    @Bean
+    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Integer, String>>
+    kafkaListenerContainerFactory2() {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory2());
+        factory.setConcurrency(3);
+        factory.getContainerProperties().setPollTimeout(3000);
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<Integer, String> consumerFactory2() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs2());
+    }
+
+    @Bean
+    public Map<String, Object> consumerConfigs2() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, "20971520");
+        props.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, "20971520");
+
+        props.put("security.protocol", "SASL_SSL");
+        props.put("sasl.jaas.config", this.kafkaSaslJaasConfig);
+        props.put("sasl.mechanism", this.kafkaSaslMechanism);
+
+        return props;
+    }
+}
